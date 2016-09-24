1aa803e71eafe78db8c3374332a42a4b0f36959f
Timi Ajiboye <timi@helloworld.ng>
2016-09-24 04:42:46 +0100

    Prelimary Setup: set up extensions and CORS

--- config/application.rb
+++ config/application.rb
@@ -18,13 +18,8 @@ Bundler.require(*Rails.groups)
 
 module Chatbot
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    config.autoload_paths += %W(#{config.root}/app/extensions)
+    config.autoload_paths += Dir["#{config.root}/app/extensions/**/"]
     config.api_only = true
   end
 end
